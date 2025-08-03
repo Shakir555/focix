08c1874deaaec117c480b11b6786a35c75967ba0
Shakir Salam <shakirsalam555@Gmail.com>
2025-08-03 22:45:11 +0800

    update login design

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
-function showTab(tabId) {
+function showTab(tabId)
+{
   document.querySelectorAll('.tab-content').forEach(div => {
     div.style.display = 'none';
   });
-  document.getElementById(tabId).style.display = 'block';
+  document.getElementsById(tabId).style.display = 'block';
 }
